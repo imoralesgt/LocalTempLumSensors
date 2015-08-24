d1fef6849d5dc77dffecab1b1c818d51ff9fc813
imoralesgt <ivan.rene.morales@gmail.com>
2015-08-24 16:47:14 -0600

    Initial commit with sensors display on OLED

--- LocalTempLum/LocalTempLum.ino
+++ LocalTempLum/LocalTempLum.ino
@@ -2,9 +2,13 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_BMP085.h>
+#include <BH1750.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_BMP085 bmp;
+BH1750 lux;
 
 #define ENABLE_DISPLAY
 
@@ -14,21 +18,38 @@ Adafruit_SSD1306 display(OLED_RESET);
 //Prototypes
 void setupDisplay(void);
 void showSplashScreen(void);
+void displaySensors(uint16_t l, float t, uint32_t p);
+void serialDisplaySensors(uint16_t l, float t, uint32_t p);
 
 void setup(){                
   #ifdef ENABLE_DISPLAY
     setupDisplay();
     showSplashScreen();
+  #else
+    Serial.begin(9600);
+    Serial.println("Serial debug mode\n");
   #endif
+  lux.begin();
+  bmp.begin();
+  delay(5000);
 
 }
 
 
 void loop() {
+  volatile uint16_t light = lux.readLightLevel();
+  volatile float temperature = bmp.readTemperature();
+  volatile uint32_t pressure = bmp.readPressure()/100;
+  
+  #ifdef ENABLE_DISPLAY
+    displaySensors(light, temperature, pressure);
+  #else
+    serialDisplaySensors(light, temperature, pressure);
+  #endif
+  
+  delay(1000);
   
 }
-
-
 
 
 void setupDisplay(void){
@@ -47,6 +68,41 @@ void showSplashScreen(void){
   display.setTextSize(2);
   display.println("  SENSOR");
   display.setTextSize(1);
-  display.println("Temperatura + Humedad");
+  //display.println("Temperatura + Humedad");
+  display.println("  Temperatura + Lux");
   display.display();
+}
+
+void displaySensors(uint16_t l, float t, uint32_t p){
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  display.println("Temperatura (Celsius)");
+  display.setTextSize(2);
+  display.print("  ");
+  display.println(t);
+/*
+  display.setTextSize(1);
+  display.println("\nPresion atmosferica");
+  display.setTextSize(2);
+  display.print(p);
+  display.println(" hPa");
+*/
+  display.setTextSize(1);
+  display.println("\nLuminosidad (luxes)");
+  display.setTextSize(2);
+  for(int i = 0; i < (9 - sizeof(l))/2; i++){
+    display.print(" ");
+  }
+  display.println(l);
+  display.display();
+}
+
+void serialDisplaySensors(uint16_t l, float t, uint32_t p){
+  Serial.print(t);
+  Serial.println(" °C");
+  Serial.print(p);
+  Serial.println(" hPa");
+  Serial.print(l);
+  Serial.println(" lux\n\n");
 }
